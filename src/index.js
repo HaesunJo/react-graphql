73e26efdcf0eb67164e47a48b1880cccf3a04317
HaesunJo <hsj30338@gmail.com>
2022-05-20 15:31:32 -0700

    set up Apollo and get the movie list

--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/App';
+import client from './apollo';
+import { ApolloProvider } from "@apollo/client";
 
+// set up apollo
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
   </React.StrictMode>
 );
 
