a7e415cf99879e3d921d9662d8a50ba8f503c979
HaesunJo <hsj30338@gmail.com>
2022-05-20 22:16:59 -0700

    called data and styled

---
+++ src/routes/Detail.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const Detail = () => {
+    const content = <div>Detail</div>;
+    return (
+      <div>{content}</div>
+    )
+  }
+  
+
+export default Detail
